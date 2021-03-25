7c00b92dd9578801cc8cc1ebfed0a8e78824e12c
Simon Larsén <slarse@slar.se>
2021-03-25 13:18:24 +0100

    Tweak deployment script

--- deploy_website.sh
+++ deploy_website.sh
@@ -10,20 +10,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [[ -z $1 ]]; then
-    INITIAL_WORKDIR="$PWD"
-else
-    INITIAL_WORKDIR="$1"
-fi
+INITIAL_WORKDIR="$1"
+GITHUB_USER="$2"
+GITHUB_TOKEN="$3"
 
-#SOURCE_REPO="https://github.com/INRIA/spoon.git" # TODO use
-SOURCE_REPO="https://github.com/slarse/spoon.git" # TODO remove
+#SOURCE_REPO_URL="https://github.com/INRIA/spoon.git" # TODO use
+SOURCE_REPO_URL="https://github.com/slarse/spoon.git" # TODO remove
 SOURCE_DIR="$INITIAL_WORKDIR/temp-spoon-clone"
 WEBSITE_SOURCE_DIR="$SOURCE_DIR/doc"
 WEBSITE_GENERATED_DIR="$WEBSITE_SOURCE_DIR/_site"
 WEBSITE_DST_DIR="$INITIAL_WORKDIR/spoon-website"
-WEBSITE_REPO="https://github.com/spoonlabs/spoonlabs.github.io"
-WEBSITE_BRANCH=main
+WEBSITE_REPO_SLUG="SpoonLabs/spoonlabs.github.io"
+WEBSITE_REPO_URL="https://github.com/$WEBSITE_REPO_SLUG"
+WEBSITE_BRANCH="main"
+
+NUM_ARGS=3
+
+function generate_website() {
+    # Generate the website
+    generate_javadoc
+
+    cd "$WEBSITE_SOURCE_DIR"
+    cp ../README.md doc_homepage.md
+
+    LATESTVERSION=`curl -s "http://search.maven.org/solrsearch/select?q=g:%22fr.inria.gforge.spoon%22+AND+a:%22spoon-core%22&core=gav" | jq -r '.response.docs | map(select(.v | match("^[0-9.]+$")) | .v )| .[0]'`
+    sed -i -e "s/^spoon_release: .*/spoon_release: $LATESTVERSION/" _config.yml
+    SNAPSHOTVERSION=`xmlstarlet sel -t -v /_:project/_:version ../pom.xml`
+    sed -i -e "s/^spoon_snapshot: .*/spoon_snapshot: \"$SNAPSHOTVERSION\"/" _config.yml
+    jekyll build
+}
 
 function generate_javadoc() {
     # Generate Javadoc and place it in the website sources
@@ -35,17 +50,6 @@ function generate_javadoc() {
     cp -r target/site/ "$WEBSITE_SOURCE_DIR"/mvnsites/spoon-core
 }
 
-function generate_website() {
-    # Generate the website (Javadoc must be generated first!)
-    cd "$WEBSITE_SOURCE_DIR"
-    cp ../README.md doc_homepage.md
-
-    LATESTVERSION=`curl -s "http://search.maven.org/solrsearch/select?q=g:%22fr.inria.gforge.spoon%22+AND+a:%22spoon-core%22&core=gav" | jq -r '.response.docs | map(select(.v | match("^[0-9.]+$")) | .v )| .[0]'`
-    sed -i -e "s/^spoon_release: .*/spoon_release: $LATESTVERSION/" _config.yml
-    SNAPSHOTVERSION=`xmlstarlet sel -t -v /_:project/_:version ../pom.xml`
-    sed -i -e "s/^spoon_snapshot: .*/spoon_snapshot: \"$SNAPSHOTVERSION\"/" _config.yml
-    jekyll build
-}
 
 function configure_git_as_github_bot() {
     git config --local user.email github-actions[bot]@users.noreply.github.com
@@ -59,7 +63,7 @@ function update_website() {
     git init -b "$WEBSITE_BRANCH"
     configure_git_as_github_bot
 
-    git remote add origin "$WEBSITE_REPO"
+    git remote add origin "$WEBSITE_REPO_URL"
     git fetch origin
 
     # this is intricate; we update the HEAD reference without updating the working directory,
@@ -71,12 +75,23 @@ function update_website() {
         echo "Nothing to commit, website up-to-date"
         return
     }
+
+    git push "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/$WEBSITE_REPO_SLUG" "$WEBSITE_BRANCH"
 }
 
-cd "$INITIAL_WORKDIR"
-mkdir "$WEBSITE_DST_DIR"
-git clone "$SOURCE_REPO" "$SOURCE_DIR"
+function main() {
+    # the main script
+    if [[ "$#" -ne "$NUM_ARGS" ]]; then
+        echo "usage: deploy_website.sh <workdir> <github_user> <github_token>"
+        exit 1
+    fi
 
-generate_javadoc
-generate_website
-update_website
+    cd "$INITIAL_WORKDIR"
+    mkdir "$WEBSITE_DST_DIR"
+    git clone "$SOURCE_REPO_URL" "$SOURCE_DIR"
+
+    generate_website
+    update_website
+}
+
+main
